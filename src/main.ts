b7109b9fe6c16a789d416d50b43f9f195bc26f2a
ADAM Mario <adam.mario@icloud.com>
2026-04-08 11:59:10 +0200

    moi

--- src/main.ts
+++ src/main.ts
@@ -14,55 +14,37 @@ enum ExitStatus {
   Success = 0,
 }
 
-async function mainHttp(port: number) {
+
+async function main(port: number) {
   try {
     const httpServer = new HttpServer(express());
-    await exits(httpServer);   
-
+    const exitSignals: NodeJS.Signals[] = ["SIGINT", "SIGTERM", "SIGQUIT"];
+    
+    if (!fs.existsSync(path.resolve(__dirname, "./public/js", "configuration.js")) && await isDbExists() === true) writeConfig(); 
+    exitSignals.map((sig) =>
+      process.on(sig, async () => {
+        try {
+          httpServer.stop();
+          logger.info("Arret du serveur OK");
+          process.exit(ExitStatus.Success);
+        } catch (error) {
+          logger.error(`Arret du serveur avec l'erreur : ${error}`);
+          process.exit(ExitStatus.Failure);
+        }
+      })
+    ); 
+    
     const app = await httpServer.createApp();
-    // app.listen(port, () => logger.info(`Serveur HTTP actif sur le port ${port}`));
-    http.createServer(app).listen(port, () => {
-      logger.info(`Serveur HTTP actif sur le port ${port}`);
+
+    http.createServer(app).listen(3000, () => {
+      logger.info(`Serveur HTTP actif sur le port ${3000}`);
     });
-  } catch (error) {
-    logger.error(`Arret du serveur avec l'erreur : ${error}`);
-    process.exit(ExitStatus.Failure);
-  }
-};
-
-
-async function exits(httpServer: HttpServer) {
-  const exitSignals: NodeJS.Signals[] = ["SIGINT", "SIGTERM", "SIGQUIT"];
-  
-  if (!fs.existsSync(path.resolve(__dirname, "./public/js", "configuration.js")) && await isDbExists() === true) writeConfig(); 
-  exitSignals.map((sig) =>
-    process.on(sig, async () => {
-      try {
-        httpServer.stop();
-        logger.info("Arret du serveur OK");
-        process.exit(ExitStatus.Success);
-      } catch (error) {
-        logger.error(`Arret du serveur avec l'erreur : ${error}`);
-        process.exit(ExitStatus.Failure);
-      }
-    })
-  );
-}
-
-async function mainHttps(port: number) {
-  try {
-    const httpServer = new HttpServer(express());
-    await exits(httpServer);   
     
-    const options = {
-      key: fs.readFileSync(path.join(__dirname, 'keys/server.key')),
-      cert: fs.readFileSync(path.join(__dirname, 'keys/server.crt'))
-    };
-    
-    const app = await httpServer.createApp();
-    
-    https.createServer(options, app).listen(port, () => {
-      logger.info(`Serveur HTTPS actif sur le port ${port}`);
+    https.createServer({
+      key: fs.readFileSync(path.join(__dirname, 'keys/server.key'), 'utf8'),
+      cert: fs.readFileSync(path.join(__dirname, 'keys/server.crt'), 'utf8')
+    }, app).listen(443, () => {
+      logger.info(`Serveur HTTPS actif sur le port ${443}`);
     });
   } catch (error) {
     logger.error(`Arret du serveur avec l'erreur : ${error}`);
@@ -71,8 +53,5 @@ async function mainHttps(port: number) {
 
 };
 
-if (process.env.NODE_ENV === 'production') 
-  mainHttps(443);
-else 
-  mainHttp(3000);
+main(3000);
   
