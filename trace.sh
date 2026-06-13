0e8d87a4596a9f1d6c4d4842056b28cf6674000c
ADAM Mario <mario.adam@inrae.fr>
2026-06-13 15:06:06 +0200

    Try it

--- trace.sh
+++ trace.sh
@@ -187,8 +187,7 @@ check_pg() {
         warning "Installing postgresql-postgis ..."
         sudo install -d /usr/share/postgresql-common/pgdg
         sudo curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
-        sudo sh -c 'echo "deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-        sudo apt update
+         sudo apt update
         sudo apt install postgresql-17-postgis-3 -y
             if ! psql --version | grep -q "psql (PostgreSQL)"; then
             exit
